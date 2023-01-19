dc7b1b5d46678076dea5590fac38dc2843cf4fc5
Hugo Pinto <hugoborgesfga@gmail.com>
2023-01-19 01:41:13 -0300

    Testing checkout step and whole pipeline

--- src/main/java/com/interview/shoppingbasket/RetailPriceCheckoutStep.java
+++ src/main/java/com/interview/shoppingbasket/RetailPriceCheckoutStep.java
@@ -1,8 +1,13 @@
 package com.interview.shoppingbasket;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class RetailPriceCheckoutStep implements CheckoutStep {
     private PricingService pricingService;
     private double retailTotal;
+    private double discountTotal;
 
     public RetailPriceCheckoutStep(PricingService pricingService) {
         this.pricingService = pricingService;
@@ -12,21 +17,27 @@ public class RetailPriceCheckoutStep implements CheckoutStep {
     public void execute(CheckoutContext checkoutContext) {
         Basket basket = checkoutContext.getBasket();
         retailTotal = 0.0;
+        discountTotal = 0.0;
+
+        final Map<String, List<Promotion>> promotionsPerProductCode = checkoutContext.getPromotions()
+            .stream()
+            .collect(Collectors.groupingBy(Promotion::getProductCode));
 
         for (BasketItem basketItem: basket.getItems()) {
             int quantity = basketItem.getQuantity();
             double price = pricingService.getPrice(basketItem.getProductCode());
             basketItem.setProductRetailPrice(price);
             retailTotal += quantity*price;
+
+            discountTotal += promotionsPerProductCode.get(basketItem.getProductCode()).stream()
+                .mapToDouble(promotion -> applyPromotion(promotion, basketItem))
+                .sum();
         }
 
-        checkoutContext.setRetailPriceTotal(retailTotal);
+        checkoutContext.setRetailPriceTotal(retailTotal - discountTotal);
     }
 
-    public double applyPromotion(Promotion promotion, BasketItem item, double price) {
-        /*
-         * Implement applyPromotion method
-         */
-        return retailTotal;
+    public double applyPromotion(Promotion promotion, BasketItem item) {
+        return promotion.computeDiscount(item);
     }
 }
--- src/test/java/com/interview/shoppingbasket/CheckoutPipelineTest.java
+++ src/test/java/com/interview/shoppingbasket/CheckoutPipelineTest.java
@@ -1,11 +1,21 @@
 package com.interview.shoppingbasket;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
+import com.interview.shoppingbasket.promotions.PercentageOffPromotion;
+import com.interview.shoppingbasket.promotions.TwoForOnePromotion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@ExtendWith(MockitoExtension.class)
 public class CheckoutPipelineTest {
 
     CheckoutPipeline checkoutPipeline;
@@ -18,6 +28,9 @@ public class CheckoutPipelineTest {
 
     @Mock
     CheckoutStep checkoutStep2;
+
+    @Mock
+    PricingService pricingService;
 
     @BeforeEach
     void setup() {
@@ -33,7 +46,51 @@ public class CheckoutPipelineTest {
 
     @Test
     void executeAllPassedCheckoutSteps() {
-        // Exercise - implement testing passing through all checkout steps
+        // 1st - Basket Consolidation Step
+        final BasketConsolidationCheckoutStep basketConsolidationStep = new BasketConsolidationCheckoutStep();
+
+        // 2nd - Promotion Computation Step
+        final Set<Promotion> promotions = new HashSet<>();
+        promotions.add(new TwoForOnePromotion("A"));
+        promotions.add(new PercentageOffPromotion("B", 50));
+        promotions.add(new PercentageOffPromotion("C", 10));
+
+        final PromotionsService promotionsService = new PromotionsServiceImpl(promotions);
+        final PromotionComputationCheckoutStep promotionStep = new PromotionComputationCheckoutStep(promotionsService);
+
+        // 3rd - Retail Price step
+        final RetailPriceCheckoutStep retailPriceStep = new RetailPriceCheckoutStep(pricingService);
+
+        // Pipeline assembly
+        checkoutPipeline.addStep(basketConsolidationStep);
+        checkoutPipeline.addStep(promotionStep);
+        checkoutPipeline.addStep(retailPriceStep);
+
+        // Creating basket and mocking prices on pricingService
+        final Basket testBasket = new Basket();
+        testBasket.add("A", "Product A", 3);
+        when(pricingService.getPrice(eq("A"))).thenReturn(100.0);
+
+        testBasket.add("B", "Product B", 10);
+        when(pricingService.getPrice(eq("B"))).thenReturn(10.0);
+
+        testBasket.add("C", "Product C", 10);
+        when(pricingService.getPrice(eq("C"))).thenReturn(50.0);
+
+        final PaymentSummary summary = checkoutPipeline.checkout(testBasket);
+        final double finalPrice = summary.getRetailTotal();
+
+        /*
+         Expecting:
+
+            | Code | Retail Price | Discount Price | Total |
+            | A    | 3x100 = 300  | 100            | 200   |
+            | B    | 10x10 = 100  | 50             | 50    |
+            | C    | 10x50 = 500  | 50             | 450   |
+
+            200 + 50 + 450 = 700
+         */
+        assertEquals(700, finalPrice);
     }
 
 }
